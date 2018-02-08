fb8ded7515d4130fc6ec3f56981da983f7ea469b
JDBar <jdbar1215@gmail.com>
2018-02-08 15:17:59 -0800

    :wrench: Correctly fetches all reviews.

--- main.py
+++ main.py
@@ -2,20 +2,18 @@ import requests
 
 ENDPOINT = "https://store.steampowered.com/appreviews/"
 
-def getAllReviews(appid="404200"):
-  """Request reviews from the Steam Web API and return them as a list.
-  \n**appid** -- The Steam App ID obtained from the game's stoe page URL
+def getSteamReviews(appid='404200'):
+  """Request reviews from the Steam Web API and return them as a list.\n
+  **appid** -- The Steam App ID obtained from the game's stoe page URL
   """
 
   def makeRequest(appid, params):
-    """Sends a request to the Steam Web API and return the response object.
-    \n**appid** -- The Steam App ID obtained from the game's Store page URL
-    \n**params** -- A dictionary used to build the Steam API query. (https://partner.steamgames.com/doc/store/getreviews)
+    """Sends a request to the Steam Web API and return the response object.\n
+    **appid** -- The Steam App ID obtained from the game's Store page URL\n
+    **params** -- An object used to build the Steam API query. (https://partner.steamgames.com/doc/store/getreviews)
     """
-    # get the data from the endpoint
-    response = requests.get(url = ENDPOINT + appid, params = params)
-    # return data from the json response
-    return response.json()
+    response = requests.get(url = ENDPOINT + appid, params = params) # get the data from the endpoint
+    return response.json() # return data extracted from the json response
     
   results = []
   params = {
@@ -28,23 +26,23 @@ def getAllReviews(appid="404200"):
   }
 
   data = makeRequest(appid, params)
-
   done = False
+
   while not done:
-    if hasattr(data, 'success') and data.success == 1:
-      if hasattr(data, 'total_reviews'):
-        # there are more pages of reviews to request
-        # increase the start offset by the number of reviews received in this response
-        params['start_offset'] += data.num_reviews
-        # add the reviews in this query to our results
-        results += data.reviews
-        # get more reviews
-        data = makeRequest(appid, params)
-      else:
-        # this is the last page of reviews
+    if 'success' in data and data['success'] == 1: # if the query was successful
+      if 'reviews' in data and len(data['reviews']) > 0: # if we received reviews
+        results += data['reviews'] # add the reviews in this query to our results
+        params['start_offset'] += data['query_summary']['num_reviews'] # increase the start offset by the number of reviews received in this response
+        data = makeRequest(appid, params) # get the next page of reviews
+      else: # there are no more reviews
         done = True
-    else:
-      # unsuccessful API call raises an error
-      raise ConnectionRefusedError("Steam Web API appreviews request was unsuccessful.")
+    elif data is None: # Steam Web API returns null if rate limit is reached.
+      done = True
+      raise ConnectionRefusedError("Steam Web API returned null. Rate limit may be exceeded.")
+    else: # unsuccessful API call raises an error
+      done = True
+      raise ConnectionError("Steam Web API appreviews request was unsuccessful.")
   
   return results
+
+print('Found {amount} results.'.format(amount=len(getSteamReviews())))
